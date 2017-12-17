eddc49fe97dbfe620841537a2c5f14e92237daad
Sven Dowideit <sven.dowideit@csiro.au>
2017-12-17 22:53:38 +0000

    Hardcoded example of data
    Signed-off-by: Sven Dowideit <sven.dowideit@csiro.au>

---
+++ data/update-data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+mkdir -p fx3/gbr1_2.0
+cd fx3/gbr1_2.0
+
+year="2017"
+for month in $(seq 11 12); do
+	for day in $(seq 18 31); do
+		file="http://dapds00.nci.org.au/thredds/fileServer/fx3/gbr1_2.0/gbr1_simple_${year}-${month}-${day}.nc"
+		echo "getting ${file}"
+		wget -t 1 --continue ${file}
+	done
+done
