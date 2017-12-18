3a19c7ccbf8f28bb5e9f45514440df9d105285a1
Sven Dowideit <sven.dowideit@csiro.au>
2017-12-18 01:39:49 +0000

    works, but needed swap to not crash - and is slooooooow
    Signed-off-by: Sven Dowideit <sven.dowideit@csiro.au>

--- data/update-data.sh
+++ data/update-data.sh
@@ -4,8 +4,8 @@ mkdir -p fx3/gbr1_2.0
 cd fx3/gbr1_2.0
 
 year="2017"
-for month in $(seq 11 12); do
-	for day in $(seq 18 31); do
+for month in $(seq 11 11); do
+	for day in $(seq 25 31); do
 		file="http://dapds00.nci.org.au/thredds/fileServer/fx3/gbr1_2.0/gbr1_simple_${year}-${month}-${day}.nc"
 		echo "getting ${file}"
 		wget -t 1 --continue ${file}
