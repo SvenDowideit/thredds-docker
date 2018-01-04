caf7eafd2dd74122c74b5311d5d29c5502020845
Sven Dowideit <sven.dowideit@csiro.au>
2018-01-04 12:10:12 +1100

    start pulling gbr4
    Signed-off-by: Sven Dowideit <sven.dowideit@csiro.au>

--- data/update-data.sh
+++ data/update-data.sh
@@ -1,48 +1,77 @@
 #!/bin/bash 
 
-outputdir="fx3/gbr1_2.0"
+function main() {
+	# GBR1
+	dir="fx3/gbr1_2.0"
+	mkdir -p "${dir}"
+	year="2017"
+	for month in $(seq -w 12 -1 10); do
+		for day in $(seq -w 31 -1 1); do
+			filename="gbr1_simple_${year}-${month}-${day}.nc"
 
-mkdir -p "${outputdir}"
+			outputpath="${dir}/${filename}"
+			file="http://dapds00.nci.org.au/thredds/fileServer/${dir}/${filename}"
 
-year="2017"
-for month in $(seq -w 12 -1 11); do
-	for day in $(seq -w 31 -1 1); do
-		file="http://dapds00.nci.org.au/thredds/fileServer/fx3/gbr1_2.0/gbr1_simple_${year}-${month}-${day}.nc"
-
-		filename="${outputdir}/gbr1_simple_${year}-${month}-${day}.nc"
-		tmpfilename="${filename}.unchecked"
-		headerfilename="${filename}.header"
-
-		if [ -e "${headerfilename}" ]; then
-			echo "Skipping ${filename}, there's header file"
-			continue
-		fi
-
-		if [ -e "${filename}" ]; then
-			echo "Already have ${filename}"
-			continue
-		fi
-
-		echo "getting ${file}"
-		wget -O ${tmpfilename} -t 1 --continue ${file}
-		if [ ! -e ${tmpfilename} ]; then
-			echo "$file didn't download"
-			continue
-		fi
-		# TODO: use ncdump to check its ok
-		pushd .
-		cd ..
-		HEADER=$(VERSION=4.6.11 docker-compose run thredds-production \
-			ncdump -h "/usr/local/tomcat/content/thredds/public/${tmpfilename}")
-		OK=$?
-		echo "are we $OK"
-		popd
-		echo "$HEADER" > "$headerfilename"
-	       	if [ "$OK" == "0" ]; then
-			echo "OK: ${filename}"
-			mv ${tmpfilename} ${filename}
-		else
-			echo "BROKEN: ${filename}"
-		fi
+			#get_netcdf_file $outputpath $file
+		done
 	done
-done
+
+	# GBR4
+	dir="fx3/gbr4_v2"
+	mkdir -p "${dir}"
+	year="2017"
+	for month in $(seq -w 12 -1 10); do
+		filename="gbr4_simple_${year}-${month}.nc"
+
+		outputpath="${dir}/${filename}"
+		file="http://dapds00.nci.org.au/thredds/fileServer/${dir}/${filename}"
+
+		get_netcdf_file $outputpath $file
+	done
+}
+
+
+################################################################
+function get_netcdf_file() {
+	local outputpath=$1
+	local fileurl=$2
+
+	local tmpoutputpath="${outputpath}.unchecked"
+	local headeroutputpath="${outputpath}.header"
+
+	if [ -e "${headeroutputpath}" ]; then
+		echo "Skipping ${outputpath}, there's header file"
+		return 0
+	fi
+
+	if [ -e "${outputpath}" ]; then
+		echo "Already have ${outputpath}"
+		return 0
+	fi
+
+	echo "getting ${fileurl}"
+	wget -O ${tmpoutputpath} -t 1 --continue 9 ${fileurl}
+	if [ ! -e ${tmpoutputpath} ]; then
+		echo "$fileurl didn't download"
+		return 9
+	fi
+	# TODO: use ncdump to check its ok
+	pushd .
+	cd ..
+	local HEADER=$(VERSION=4.6.11 docker-compose run thredds-production \
+		ncdump -h "/usr/local/tomcat/content/thredds/public/${tmpoutputpath}")
+	local OK=$?
+	echo "are we $OK"
+	popd
+	echo "$HEADER" > "$headeroutputpath"
+       	if [ "$OK" == "0" ]; then
+		echo "OK: ${outputpath}"
+		mv ${tmpoutputpath} ${outputpath}
+	else
+		echo "BROKEN: ${outputpath}"
+		return $OK
+	fi
+	return 0
+}
+
+main
